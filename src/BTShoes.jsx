33d79308ea4c68477695b22c9b1f4d49c5a93399
TRINHQUOCDUAN <tqduan1402@gmail.com>
2026-05-26 00:02:43 +0700

    cap nhat

--- src/BTShoes.jsx
+++ src/BTShoes.jsx
@@ -5,45 +5,26 @@ import { ProductDetail } from './ProductDetail'
 import { ProductCart } from './ProductCart'
 
 export const BTShoes = () => {
-    // State quản lý chi tiết sản phẩm đang xem
-    const [prdDetail, setPrdDetail] = useState({
-        sizes: [32, 33, 34, 35],
-        id: 2,
-        name: 'vans old school',
-        alias: 'vans-old-school',
-        price: 200,
-        description: 'about this shoe:Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
-        size: '[32,33,34,35]',
-        shortDescription: 'about this shoe:Lorem ipsum dolor sit amet...',
-        quantity: 200,
-        deleted: false,
-        categories: '[{"id": "VANS_CONVERSE","category":"VANS_CONVERSE"}]',
-        relatedProducts: '[3,2,1]',
-        feature: true,
-        image: 'https://apistore.cybersoft.edu.vn/images/van-old-school.png',
-        imgLink: 'https://apistore.cybersoft.edu.vn/images/van-old-school.png',
-    })
+    // Sửa lỗi crash '.join()' bằng cách lấy phần tử đầu tiên làm mặc định đầy đủ thuộc tính
+    const [prdDetail, setPrdDetail] = useState(dataShoes[0] || {})
 
-    // State quản lý danh sách sản phẩm trong giỏ hàng
     const [cart, setCart] = useState([
         {
-            ...prdDetail,
-            cartQuantity: 1,
-        },
+            ...dataShoes[0],
+            cartQuantity: 1 // Số lượng mặc định ban đầu trong giỏ hàng
+        }
     ])
 
-    // Thay đổi sản phẩm hiển thị ở bảng chi tiết
     const handlePrdDetail = (newPrd) => {
         setPrdDetail(newPrd)
     }
 
-    // Thêm sản phẩm vào giỏ hàng
     const handleAddToCart = (prd) => {
         const index = cart.findIndex((item) => item.id === prd.id)
 
         if (index === -1) {
-            const newCartItem = { ...prd, cartQuantity: 1 }
-            setCart([...cart, newCartItem])
+            // Nếu chưa có, clone object ra và gán số lượng bằng 1 để tránh lỗi tham chiếu trực tiếp dataJson
+            setCart([...cart, { ...prd, cartQuantity: 1 }]) 
         } else {
             const newCart = [...cart]
             newCart[index].cartQuantity += 1
@@ -51,17 +32,17 @@ export const BTShoes = () => {
         }
     }
 
-    // Xóa sản phẩm khỏi giỏ hàng
     const handleDeleteCartItem = (prdId) => {
         setCart(cart.filter((item) => item.id !== prdId))
     }
 
-    // Tăng / giảm số lượng sản phẩm trực tiếp trong giỏ hàng
+    // --- HÀM TĂNG GIẢM SỐ LƯỢNG CHO ĐÚNG YÊU CẦU ---
     const handleQuantityChange = (id, amount) => {
         const newCart = [...cart]
         const index = newCart.findIndex((item) => item.id === id)
 
         if (index !== -1) {
+            // Tăng hoặc giảm dựa trên amount (1 hoặc -1), tối thiểu luôn là 1 sản phẩm
             newCart[index].cartQuantity = Math.max(1, newCart[index].cartQuantity + amount)
             setCart(newCart)
         }
@@ -69,10 +50,11 @@ export const BTShoes = () => {
 
     return (
         <div className="mt-10 container mx-auto">
-            <h1 className="text-center text-2xl font-medium mb-5">Shoes Shop</h1>
+            <h1 className="text-center text-2xl font-medium">Shoes Shop</h1>
 
             <ProductDetail product={prdDetail} />
 
+            {/* QUAN TRỌNG: Phải truyền hàm handleQuantityChange vào đây để file ProductCart nhận được */}
             <ProductCart 
                 cart={cart} 
                 handleDeleteCartItem={handleDeleteCartItem} 
--- src/ProductCart.jsx
+++ src/ProductCart.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
 export const ProductCart = (props) => {
-    const { cart, handleDeleteCartItem } = props
-
-    console.log({ cart })
+    // Nhận các state và hàm xử lý được truyền từ BTShoes xuống
+    const { cart, handleDeleteCartItem, handleQuantityChange } = props
 
     return (
         <div id="product-cart-modal" className="modal" tabIndex={-1}>
@@ -11,12 +10,7 @@ export const ProductCart = (props) => {
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title">Giỏ hàng</h5>
-                        <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                        />
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
                     </div>
                     <div className="modal-body">
                         <table className="table">
@@ -32,31 +26,34 @@ export const ProductCart = (props) => {
                                 </tr>
                             </thead>
                             <tbody>
-                                {/* DS sản phẩm trong giỏ hàng */}
                                 {cart.map((item, index) => (
                                     <tr key={item.id}>
                                         <td>{index + 1}</td>
                                         <td>{item.name}</td>
                                         <td>
-                                            <img
-                                                src={item.image}
-                                                alt={item.name}
-                                                className="w-20"
-                                            />
+                                            <img src={item.image} alt={item.name} style={{ width: '50px' }} />
                                         </td>
                                         <td>{item.price}</td>
                                         <td>
-                                            <button className="btn btn-outline-danger">-</button>{' '}
-                                            {item.cartQuantity}{' '}
-                                            <button className="btn btn-outline-success">+</button>
+                                            {/* Nút giảm số lượng */}
+                                            <button 
+                                                className="btn btn-outline-danger" 
+                                                onClick={() => handleQuantityChange(item.id, -1)}
+                                            >-</button>
+                                            
+                                            <span className="mx-2">{item.cartQuantity}</span>
+                                            
+                                            {/* Nút tăng số lượng */}
+                                            <button 
+                                                className="btn btn-outline-success" 
+                                                onClick={() => handleQuantityChange(item.id, 1)}
+                                            >+</button>
                                         </td>
                                         <td>{item.price * item.cartQuantity}</td>
                                         <td>
                                             <button
                                                 className="btn btn-danger"
-                                                onClick={() => {
-                                                    handleDeleteCartItem(item.id)
-                                                }}
+                                                onClick={() => handleDeleteCartItem(item.id)}
                                             >
                                                 Xóa
                                             </button>
